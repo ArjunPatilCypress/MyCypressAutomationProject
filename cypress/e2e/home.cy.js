c131a1277f5e7f521cf159eea4595d08e0a77d1b
ArjunPatilCypress <Arjunnipani@gmail.com>
2023-08-16 00:01:02 -0500

    My First Commit

--- cypress/e2e/home.cy.js
+++ cypress/e2e/home.cy.js
@@ -3,33 +3,34 @@ import Home from "../pages/blog.js"
 
   
  describe("testing blog page", () => {
-   beforeEach(() => {
+  //  beforeEach(() => {
     
-   })
+  //  })
   
-   it("should visit the blog page", () => {
-     cy.title().should("eq", "Blog - Poco theme")
-   })
+  //  it("should visit the blog page", () => {
+  //    cy.title().should("eq", "Blog - Poco theme")
+  //  })
   
-   it("should have correct category names", () => {
-     blog.getFirstCategoryButton().should("contain.text", "Business")
-     blog.getSecondCategoryButton().should("contain.text", "Electronics")
-     blog.getThirdCategoryButton().should("contain.text", "Technology")
-   })
+  //  it("should have correct category names", () => {
+  //    blog.getFirstCategoryButton().should("contain.text", "Business")
+  //    blog.getSecondCategoryButton().should("contain.text", "Electronics")
+  //    blog.getThirdCategoryButton().should("contain.text", "Technology")
+  //  })
 
    it('to test api first approach',()=>{
 
-    cy.request({
-        method: 'POST', 
-        url: 'https://reqres.in/api/users', 
-        body: {
-                "name": "morpheus",
-                "job": "leader"
-        }
-      }).then( ({ status }) => {
+    // cy.request({
+    //     method: 'POST', 
+    //     url: 'https://reqres.in/api/users', 
+    //     body: {
+    //             "name": "morpheus",
+    //             "job": "leader"
+    //     }
+    //   }).then( ({ status }) => {
         
-        expect(status).to.eq(201)
-      })
+    //     expect(status).to.eq(201)
+    //   })
+    cy.log('MyTest');
 
 })
  })
